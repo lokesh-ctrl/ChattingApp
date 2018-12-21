d9509d57e3c401da10766cb897dbcac59da1974a
= <rlokesh@aconex.com>
2018-12-21 11:51:03 +0530

    Create ChatAppComponent and ChatScreenComponent
    ChatAppComponent is root component. ChatScreenComponent is for ChatScreen.

---
+++ app/components/ChatApp/ChatAppComponent.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ChatScreenComponent from './../ChatScreen/ChatScreenComponent'
+
+const ChatAppComponent = props => {
+    return <ChatScreenComponent/>
+}
+
+export default ChatAppComponent
---
+++ app/components/ChatScreen/ChatScreenComponent.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {View} from 'react-native'
+import MessageFormContainer from './MessageForm/MessageFormContainer'
+
+const ChatScreenComponent = ()=>
+    <View>
+        <MessageFormContainer/>
+    </View>
+
+export default ChatScreenComponent
