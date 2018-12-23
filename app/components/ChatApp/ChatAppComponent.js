69229dafd13581de6150423e40ae165de3506148
lokicherry <rongali.lokesh@gmail.com>
2018-12-23 12:42:39 +0530

    Take permission for loading contacts from the device

--- app/components/ChatApp/ChatAppComponent.js
+++ app/components/ChatApp/ChatAppComponent.js
@@ -1,8 +1,50 @@
 import React from 'react'
 import ChatScreenComponent from './../ChatScreen/ChatScreenComponent'
+import DeviceInfo from 'react-native-device-info';
+import RNSimData from 'react-native-sim-data'
+import Contacts from 'react-native-contacts'
+import { PermissionsAndroid } from 'react-native';
+import {View,Button} from "react-native";
 
-const ChatAppComponent = props => {
-    return <ChatScreenComponent/>
+
+class ChatAppComponent extends React.Component{
+    constructor(props){
+        super(props);
+        this.getContacts = this.getContacts.bind(this)
+    }
+    componentWillMount() {
+        this.getContacts()
+    }
+
+    async requestContactsPermission() {
+            const granted = await PermissionsAndroid.request(
+                PermissionsAndroid.PERMISSIONS.READ_CONTACTS,
+                {
+                    title: 'ChatApp contacts permission',
+                    message: 'ChatApp need access to your contacts so that you send messages to your contacts.'
+                }
+            )
+            return granted === PermissionsAndroid.RESULTS.GRANTED
+        }
+        getContacts = () => {
+            this.requestContactsPermission()
+                .then(function (didGetPermission: boolean) {
+                    if (didGetPermission) {
+                        Contacts.getAll((err, contacts) => {
+                            if (err) throw err;
+                            alert("We got some contacts!")
+                            console.log(contacts)
+                        })
+                    } else {
+                        alert("Oh no no permissions!")
+                    }
+                })
+        }
+    render() {
+        return (
+            <View>
+            <ChatScreenComponent/>
+            </View>)
+    }
 }
-
 export default ChatAppComponent
