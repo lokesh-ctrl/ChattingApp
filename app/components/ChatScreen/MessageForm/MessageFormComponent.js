32f3b63255a0e483b696fda26f70402ad2ff2b83
GAnjali <gaddam.anjali@oracle.com>
2019-01-04 15:35:43 +0530

    changes rendering messages and Adds styles to chat screen

--- app/components/ChatScreen/MessageForm/MessageFormComponent.js
+++ app/components/ChatScreen/MessageForm/MessageFormComponent.js
@@ -2,7 +2,8 @@ import React, {Component} from 'react'
 import {FlatList, KeyboardAvoidingView, Platform, Text, TextInput, TouchableOpacity, View} from 'react-native';
 import PropTypes from 'prop-types';
 import {Header, SafeAreaView} from 'react-navigation';
-import firebaseService from '../../../services/FirebaseService'
+import styles from '../../../Styles/styleSheet';
+
 
 class MessageFormComponent extends Component {
     constructor(props) {
@@ -10,35 +11,33 @@ class MessageFormComponent extends Component {
         this.state = {
             messages: []
         };
-        let that = this;
-        let conversationKey = 'Users/' + this.props.senderNumber + '/' + 'Conversations/' + this.props.receiverNumber;
-        let firebaseRef = firebaseService.database().ref(conversationKey);
-        firebaseRef.on("value", function (dataSnapshot) {
-            let messages = []
-            dataSnapshot.forEach(function (child) {
-                let message = child.val()
-                console.log(message)
-                messages.push(message)
-            })
-            that.setState({messages: messages})
-            console.log(that.state.messages)
-        });
         this.handleButtonPress = () => {
             this.props.sendMessage(this.props.message, this.props.senderNumber, this.props.receiverNumber)
-        };
+        }
         this.handleMessageChange = (message) => {
             message => this.setState({typing: message})
             this.props.updateMessage(message)
         };
     }
 
+    componentDidMount() {
+        this.setState({messages: this.props.messages});
+    }
 
     renderItem({item}) {
+        let messageboxstyle;
+        let messagetextstyle;
+        if(item._id === 1) {
+            messageboxstyle = styles.senderMessageContainer;
+            messagetextstyle = styles.senderMessage;
+        }else {
+            messageboxstyle =styles.receiverMessageContainer;
+            messagetextstyle = styles.receiverMessage;
+        }
         return (
-            <View style={styles.row}>
+            <View style={[messageboxstyle,styles.chatBox]}>
                 <View style={styles.rowText}>
-                    <Text style={styles.sender}>{item._id}</Text>
-                    <Text style={styles.message}>{item.text}</Text>
+                    <Text style={messagetextstyle}>{item.text}</Text>
                 </View>
             </View>
         );
@@ -46,7 +45,7 @@ class MessageFormComponent extends Component {
 
     renderFlatList() {
         return (
-            <View>
+            <View style={styles.container}>
                 <FlatList
                     data={this.state.messages}
                     renderItem={this.renderItem}
@@ -59,40 +58,42 @@ class MessageFormComponent extends Component {
             </View>
         );
     }
+
     render() {
         const keyboardVerticalOffset = Platform.OS === 'ios' ? Header.HEIGHT + 20 : 0;
         const padding = Platform.OS === 'ios' ? "padding" : '';
-            if(this.state.messages) {
-                this.renderFlatList();
-            }
-            return(
-                <View>
-                    <KeyboardAvoidingView
-                        keyboardVerticalOffset={keyboardVerticalOffset}
-                        behavior={padding}
-                    >
-                        <SafeAreaView forceInset={{bottom: 'never'}}>
-                            <View>
-                                <TextInput
-                                    value={this.props.message}
-                                    onChangeText={this.handleMessageChange}
-                                    underlineColorAndroid="transparent"
-                                    placeholder="Type something nice"
-                                />
-                                <TouchableOpacity onPress={this.handleButtonPress}>
-                                    <Text>Send</Text>
-                                </TouchableOpacity>
-                            </View>
-                        </SafeAreaView>
-                    </KeyboardAvoidingView>
-                </View>
-            )
+        if(this.state.messages) {
+            this.renderFlatList();
+        }
+        return(
+            <View style={styles.container}>
+                <KeyboardAvoidingView
+                    keyboardVerticalOffset={keyboardVerticalOffset}
+                    behavior={padding}
+                >
+                    <SafeAreaView forceInset={{bottom: 'never'}}>
+                        <View style={styles.footer}>
+                            <TextInput
+                                value={this.props.message}
+                                style={styles.input}
+                                onChangeText={this.handleMessageChange}
+                                underlineColorAndroid="transparent"
+                                placeholder="Type something nice"
+                            />
+                            <TouchableOpacity onPress={this.handleButtonPress}>
+                                <Text style={styles.send}>Send</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </SafeAreaView>
+                </KeyboardAvoidingView>
+            </View>
+        )
     }
 }
 MessageFormComponent.propTypes = {
     sendMessage: PropTypes.func.isRequired,
     message: PropTypes.string.isRequired,
-    updateMessage: PropTypes.func.isRequired
+    updateMessage:PropTypes.func.isRequired
 }
 export default MessageFormComponent;
 
